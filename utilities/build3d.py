cc95ec383f1369d55efc7f656e77589b37c75504
Greyson Christoforo <grey@christoforo.net>
2022-04-14 00:01:59 +0100

    2->3d mask generator works

--- utilities/build3d.py
+++ utilities/build3d.py
@@ -2,18 +2,19 @@
 import cadquery
 from cadquery import CQ, cq
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 import ezdxf
 import itertools
 
 
 class TwoDToThreeD(object):
-    dxf_filepath = Path.cwd().parent / "oxford" / "master.dxf"
+    sources: List[Path]
+    stacks: List[Dict]
     # dxf_filepath = Path(__file__).parent.parent / "oxford" / "master.dxf"  # this makes CQ-editor sad because __file__ is not defined
 
-    def __init__(self, instructions, source: Path = dxf_filepath):
-        self.stacks = instructions
-        self.dxf_filepath = source
+    def __init__(self, instructions: List[Dict], sources: List[Path]):
+        self.stacks: List[Dict] = instructions
+        self.sources: List[Path] = sources
 
     def build(self, stacks_to_build: List[str] = [""]):
         if stacks_to_build == [""]:  # build them all by default
@@ -24,10 +25,12 @@ class TwoDToThreeD(object):
             if stack_instructions["name"] in stacks_to_build:
                 for stack_layer in stack_instructions["layers"]:
                     drawing_layers_needed += stack_layer["drawing_layer_names"]
+                    if "edge_case" in stack_layer:
+                        drawing_layers_needed.append(stack_layer["edge_case"])
         drawing_layers_needed_unique = list(set(drawing_layers_needed))
 
         # all the wires we'll need here
-        wires = self.get_wires(self.dxf_filepath, drawing_layers_needed_unique)
+        wires = self.get_wires(self.sources, drawing_layers_needed_unique)
 
         stacks = {}
         for stack_instructions in self.stacks:
@@ -35,168 +38,93 @@ class TwoDToThreeD(object):
             asy.name = stack_instructions["name"]
             z_base = 0
             for stack_layer in stack_instructions["layers"]:
-                these_wires = []
-                for drawing_layer_name in stack_layer["drawing_layer_names"]:
-                    these_wires += wires[drawing_layer_name]
                 t = stack_layer["thickness"]
-                wp = CQ().add(these_wires).toPending().extrude(t).translate([0, 0, z_base])
+                boundary_layer_name = stack_layer["drawing_layer_names"][0]  # boundary layer must always be the first one listed
+                w0 = wires[boundary_layer_name][0]
+                wp = CQ().sketch().face(w0)
+                for w in wires[boundary_layer_name][1::]:
+                    wp = wp.face(w, mode="s")
+                wp = wp.finalize().extrude(t)  # the workpiece is now made
+                wp = wp.faces(">Z").sketch()
+                if "array" in stack_layer:
+                    array_points = stack_layer["array"]
+                else:
+                    array_points = [None]
+                for point in array_points:
+                    for drawing_layer_name in stack_layer["drawing_layer_names"][1::]:
+                        some_wires = wires[drawing_layer_name]
+                        for awire in some_wires:
+                            if point is None:
+                                wp = wp.face(awire)
+                            else:
+                                wp = wp.face(awire.located(cadquery.Location(cadquery.Vector(point[0], point[1], 0))))
+
+                # this handles the keepout regions at the edges of the holder
+                if "edge_case" in stack_layer:
+                    edge_wire = wires[stack_layer["edge_case"]][0]
+                    wp = wp.face(edge_wire, mode="i")
+                wp = wp.finalize().cutThruAll()
+
+                asy.add(wp.translate([0, 0, z_base]), name=stack_layer["name"], color=cadquery.Color(stack_layer["color"]))
                 z_base = z_base + t
-                asy.add(wp, name=stack_layer["name"], color=cadquery.Color(stack_layer["color"]))
             stacks[stack_instructions["name"]] = asy
         return stacks
         # asy.save(str(Path(__file__).parent / "output" / f"{stack_instructions['name']}.step"))
         # cq.Shape.exportBrep(cq.Compound.makeCompound(itertools.chain.from_iterable([x[1].shapes for x in asy.traverse()])), str(Path(__file__).parent / "output" / "badger.brep"))
 
-    def get_wires(self, dxf_filepath: Path, layer_names: List[str] = []) -> List[cq.Workplane]:
+    def get_wires(self, dxf_filepaths: List[Path], layer_names: List[str] = []) -> List[cq.Workplane]:
         """returns the wires from the given dxf layers"""
         # list of of all layers in the dxf
-        file_path_str = str(dxf_filepath)
-        dxf = ezdxf.readfile(file_path_str)
-        all_layers = set(dxf.modelspace().groupby(dxfattrib="layer").keys())
+        layer_sets = []
+        for filepath in dxf_filepaths:
+            file_path_str = str(filepath)
+            dxf = ezdxf.readfile(file_path_str)
+            layer_sets.append(set(dxf.modelspace().groupby(dxfattrib="layer").keys()))
+
+        if len(layer_sets) > 1:
+            bad_intersection = set.intersection(*layer_sets)
+            if bad_intersection:
+                raise ValueError(f"Identical layer names found in multiple drawings: {bad_intersection}")
         wires = {}
         for layer_name in layer_names:
-            to_exclude = list(all_layers - set((layer_name,)))
-            wires[layer_name] = cadquery.importers.importDXF(file_path_str, exclude=to_exclude).wires().vals()
+            for i, layer_set in enumerate(layer_sets):
+                if layer_name in layer_set:
+                    which_file = dxf_filepaths[i]
+                    break
+            to_exclude = list(layer_set - set((layer_name,)))
+            wires[layer_name] = cadquery.importers.importDXF(which_file, exclude=to_exclude).wires().vals()
 
         return wires
 
-    # for name, layer in
-    # to_exclude = [k for k in dxf_layernames if layername != k]
-
-    # return(dxf_obj.wires())
-
-
-#   def make_heater_plate(self):
-#     heater = CQ().add(self.base_plate).toPending().extrude(self.heater_t)
-#     heater = heater.faces(">Z[-1]").workplane().add(self.plate_mounts).translate((0,0,self.heater_t)).toPending().cutBlind(-self.screw_depth)
-
-#     heater = heater.translate((0,0,-self.heater_t-self.cu_base_t-self.pcb_thickness/2-self.pcb_spacer_h))
-#     return (heater)
-
-#   def make_tower_plate(self):
-#     towers = CQ().add(self.cu_base).toPending().extrude(self.cu_base_t)
-#     towers = towers.faces("<Z[-2]").workplane().add(self.cu_towers).translate((0,0,self.cu_base_t)).toPending().extrude(self.cu_tower_h)
-#     towers = towers.faces("<Z[-2]").workplane().add(self.cu_nubs).translate((0,0,self.cu_base_t)).toPending().extrude(self.cu_nub_h)
-#     towers = towers.add(self.plate_mounts).toPending().cutThruAll()
-#     towers = towers.add(self.cu_dowel_pf).toPending().cutThruAll()
-
-#     towers = towers.translate((0,0,-self.cu_base_t-self.pcb_thickness/2-self.pcb_spacer_h))
-#     return (towers)
-
-#   def get_ventscrews_a(self):
-#     ventscrew = self.vent_screw.translate((0,0,-1.7))
-#     ventscrews = CQ().pushPoints(self.screw_spots).eachpoint(lambda loc: ventscrew.val().moved(loc), True)
-#     return ventscrews
-
-#   def get_ventscrews_b(self):
-#     ventscrew = self.vent_screw.translate((0,0,3.3))
-#     ventscrews = CQ().pushPoints(self.screw_spots).eachpoint(lambda loc: ventscrew.val().moved(loc), True)
-#     return ventscrews
-
-#   def make_reservation(self, do_ventscrews:bool=False):
-#     if do_ventscrews:
-#       vss = self.get_ventscrews_a()
-#     sr = CQ().box(self.reserve_xy,self.reserve_xy,self.reserve_h,centered=(True,True,False))
-#     sr = sr.translate((0,0,-self.pcb_thickness-self.pcb_spacer_h-self.cu_base_t))
-#     wires = CQ().box(self.wire_slot_depth, 2.54*20, 2.54*2, centered=(False, True, False)).translate((-self.reserve_xy/2,0,self.wire_slot_z+self.pcb_thickness/2))
-#     wiresA = wires.translate((0, self.wire_slot_ofasyfset,0))
-#     wiresB = wires.translate((0,-self.wire_slot_offset,0))
-#     sr = sr.cut(wiresA).cut(wiresB)
-#     if do_ventscrews:
-#       sr = sr.add(vss)
-#     # these next two lines are very expensive (and optional)!
-#     sr = sr.add(self.get_pcb())
-#     #sr = CQ().union(sr)
-#     return sr
-
-#   def make_silicone(self):
-#     silicone = CQ().add(self.silicone).toPending().extrude(self.silicone_t)
-#     silicone = silicone.translate((0,0,-self.pcb_thickness/2-self.pcb_spacer_h+self.cu_tower_h))
-#     return silicone
-
-#   def make_dowels(self):
-#     dowels = CQ().add(self.dowels).toPending().extrude(self.dowel_height)
-#     dowels = dowels.translate((0,0,-self.pcb_thickness/2-self.pcb_spacer_h-sel#         c = cq.Compound.makeCompound(shapes)
-#     cu_tower_h+self.silicone_working_t))
-#     return glass
-
-#   def make_spacer_pcb(self):
-#     spacer = CQ().add(self.spacer_pcb).toPending().extrude(self.pcb_spacer_h)
-#     spacer = spacer.translate((0, 0, -self.pcb_thickness/2-self.pcb_spacer_h))
-#     return (spacer)
-
-#   def get_pcb(self):
-#     return self.pcb
-
-#   def make_pusher_plate(self):
-#     pusher = CQ().add(self.pusher_plate).toPending().extrude(self.pusher_t)
-#     pusher = pusher.add(self.plate_mounts).toPending().cutThruAll()
-#     pusher = pusher.translate((0,0,self.pcb_thickness/2+self.slots_t + self.silicone_working_t))
-#     return pusher
-
-#   def make_slot_plate(self):
-#     slots = CQ().add(self.slot_plate).toPending().extrude(self.slots_t)
-#     slots = slots.add(self.plate_mounts).toPending().cutThruAll()
-#     slots = slots.translate((0,0,self.pcb_thickness/2))
-#     return (slots)            # wp = CQ()
-#     spacer = s.make_spacer_pcb()
-#     asy.add(spacer, name="spacer", color=cadquery.Color("DARKGREEN"))
-
-#     # the towerplate
-#     tp = self.make_tower_plate()
-#     asy.add(tp, name="towers", color=cadquery.Color("GOLDENROD"))#         c = cq.Compound.makeCompound(shapes)
-
-
-#     # dowels
-#     dwl = self.make_dowels()
-#     asy.add(dwl, name="dowels", color=cadquery.Color("BLACK"))
-
-#     # silicone
-#     sil = self.make_silicone()
-#     asy.add(sil, name="silicone", color=cadquery.Color("WHITE"))
-
-#     # glass
-#     glass = self.make_glass()
-#     asy.add(glass, name="glass", color=cadquery.Color("SKYBLUE"))
-
-#     # the heater base plate
-#     heater = s.make_heater_plate()Yeah, I think the same basic design should hopefully be good for both single ones and tiled over the big cluster area (with the special cluster features added in). I guess that thicker support layer should be the one that we bend.
-
-#     asy.add(heater, name="heater", color=cadquery.Color("MATRAGRAY"))
-
-#     # the spring pin PCB
-#     pcb = self.get_pcb()
-#     asy.add(pcb, name="pcb", color=cadquery.Color("brown"))
-
-#     if do_ventscrews:
-#       # the vent screws
-#       vss_a = self.get_ventscrews_a()
-#       vss_b = self.get_ventscrews_b()
-#       asy.add(vss_a.add(vss_b), name="ventscrew")
-
-#     # the alignment slot plate
-#     slots = self.make_slot_plate()
-#     asy.add(slots, name="sample_slots", color=cadquery.Color("GRAY45"))
-
-#     pusher = self.make_pusher_plate()
-#     asy.add(pusher, name="pusher", color=cadquery.Color("GRAY28"))
-
-#     reserve = self.make_reservation(do_ventscrews=do_ventscrews)
-#     asy.add(reserve, name="space_reservation")
-
-
-#     return asy
-
 
 def main():
-    instructions = [
+    # define where we'll read shapes from
+    sources = [
+        Path.cwd().parent / "oxford" / "master.dxf",
+        Path.cwd().parent / "oxford" / "derivatives" / "5x5_cluster_master.dxf",
+    ]
+
+    support_thickness = 0.65
+    feature_thickness = 0.2
+    shim_thickness = 0.5
+
+    support_color = "GOLDENROD"
+    feature_color = "GRAY28"
+    shim_color = "DARKGREEN"
+
+    # define how we'll tile things
+    spacing5 = 30
+    array5 = [(x * spacing5, y * spacing5) for x, y in itertools.product(range(-2, 3), range(-2, 3))]
+
+    instructions = []
+    instructions.append(
         {
-            "name": "inner_active_mask_stack",
+            "name": "active_mask_stack",
             "layers": [
                 {
                     "name": "active_support",
-                    "color": "GOLDENROD",
-                    "thickness": 0.6,
+                    "color": support_color,
+                    "thickness": support_thickness,
                     "drawing_layer_names": [
                         "glass_extents",
                         "aggressive_support_active",
@@ -204,8 +132,8 @@ def main():
                 },
                 {
                     "name": "active_feature",
-                    "color": "BLACK",
-                    "thickness": 0.2,
+                    "color": feature_color,
+                    "thickness": feature_thickness,
                     "drawing_layer_names": [
                         "glass_extents",
                         "active_layer",
@@ -213,22 +141,64 @@ def main():
                 },
                 {
                     "name": "spacer_shim",
-                    "color": "DARKGREEN",
-                    "thickness": 0.1,
+                    "color": shim_color,
+                    "thickness": shim_thickness,
                     "drawing_layer_names": [
                         "glass_extents",
                         "spacer_shim",
                     ],
                 },
             ],
-        },
+        }
+    )
+
+    instructions.append(
+        {
+            "name": "active_mask_stack_5x5",
+            "layers": [
+                {
+                    "name": "active_support",
+                    "color": support_color,
+                    "thickness": support_thickness,
+                    "drawing_layer_names": [
+                        "outline_5x5",
+                        "aggressive_support_active",
+                    ],
+                    "array": array5,
+                },
+                {
+                    "name": "active_feature",
+                    "color": feature_color,
+                    "thickness": feature_thickness,
+                    "drawing_layer_names": [
+                        "outline_no_alignment_5x5",
+                        "active_layer",
+                    ],
+                    "array": array5,
+                },
+                {
+                    "name": "spacer_shim",
+                    "color": shim_color,
+                    "thickness": shim_thickness,
+                    "drawing_layer_names": [
+                        "outline_no_alignment_5x5",
+                        "spacer_shim",
+                    ],
+                    "edge_case": "inner_outline_5x5",
+                    "array": array5,
+                },
+            ],
+        }
+    )
+
+    instructions.append(
         {
             "name": "metal_mask_stack",
             "layers": [
                 {
                     "name": "metal_support",
-                    "color": "GOLDENROD",
-                    "thickness": 0.6,
+                    "color": support_color,
+                    "thickness": support_thickness,
                     "drawing_layer_names": [
                         "glass_extents",
                         "aggressive_metal_support_tc_metal",
@@ -238,8 +208,8 @@ def main():
                 },
                 {
                     "name": "metal_feature",
-                    "color": "BLACK",
-                    "thickness": 0.2,
+                    "color": feature_color,
+                    "thickness": feature_thickness,
                     "drawing_layer_names": [
                         "glass_extents",
                         "tc_metal",
@@ -249,18 +219,63 @@ def main():
                 },
                 {
                     "name": "spacer_shim_thin",
-                    "color": "DARKGREEN",
-                    "thickness": 0.1,
+                    "color": shim_color,
+                    "thickness": shim_thickness,
                     "drawing_layer_names": [
                         "glass_extents",
                         "spacer_shim_thin",
                     ],
                 },
             ],
-        },
-    ]
+        }
+    )
 
-    ttt = TwoDToThreeD(instructions=instructions)
+    instructions.append(
+        {
+            "name": "metal_mask_stack_5x5",
+            "layers": [
+                {
+                    "name": "metal_support",
+                    "color": support_color,
+                    "thickness": support_thickness,
+                    "drawing_layer_names": [
+                        "outline_5x5",
+                        "aggressive_metal_support_tc_metal",
+                        "aggressive_metal_support_small_upper",
+                        "aggressive_metal_support_large_lower",
+                    ],
+                    "edge_case": "inner_outline_5x5",
+                    "array": array5,
+                },
+                {
+                    "name": "metal_feature",
+                    "color": feature_color,
+                    "thickness": feature_thickness,
+                    "drawing_layer_names": [
+                        "outline_no_alignment_5x5",
+                        "tc_metal",
+                        "pixel_electrodes_small_upper",
+                        "pixel_electrodes_large_lower",
+                    ],
+                    "edge_case": "inner_outline_5x5",
+                    "array": array5,
+                },
+                {
+                    "name": "spacer_shim_thin",
+                    "color": shim_color,
+                    "thickness": shim_thickness,
+                    "drawing_layer_names": [
+                        "outline_no_alignment_5x5",
+                        "spacer_shim_thin",
+                    ],
+                    "edge_case": "inner_outline_5x5",
+                    "array": array5,
+                },
+            ],
+        }
+    )
+
+    ttt = TwoDToThreeD(instructions=instructions, sources=sources)
     asys = ttt.build()
     # asy: cadquery.Assembly = list(asys.values())[0]  # TODO:take more than the first value
 
@@ -283,12 +298,12 @@ def main():
         else:
             # save assembly
             asy.save(str(Path(__file__).parent / "output" / f"{asy.name}.step"))
-            cadquery.exporters.assembly.exportCAF(asy, str(Path(__file__).parent / "output" / f"{asy.name}.std"))
-            cq.Shape.exportBrep(cq.Compound.makeCompound(itertools.chain.from_iterable([x[1].shapes for x in asy.traverse()])), str(Path(__file__).parent / "output" / f"{asy.name}.brep"))
+            # cadquery.exporters.assembly.exportCAF(asy, str(Path(__file__).parent / "output" / f"{asy.name}.std"))
+            # cq.Shape.exportBrep(cq.Compound.makeCompound(itertools.chain.from_iterable([x[1].shapes for x in asy.traverse()])), str(Path(__file__).parent / "output" / f"{asy.name}.brep"))
 
             save_indivitual_stls = False
-            save_indivitual_steps = True
-            save_indivitual_breps = True
+            save_indivitual_steps = False
+            save_indivitual_breps = False
 
             # save them
             for key, val in asy.traverse():
